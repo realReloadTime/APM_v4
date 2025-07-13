3416a5185ac6378c7eda059377bbcdc5af76fc00
realReloadTime <max.shilcko@yandex.ru>
2025-07-13 22:46:29 +0700

    Определена структура проекта, начальные настройки БД

--- apm_project/settings.py
+++ apm_project/settings.py
@@ -74,8 +74,16 @@ WSGI_APPLICATION = 'apm_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apm_db',
+        'USER': 'apm_db_user',
+        'PASSWORD': '8+Y wDB*(ZUPJo_#kgtq',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'isolation_level': 'read committed',
+        },
+        'CONN_MAX_AGE': 0,
     }
 }
 
