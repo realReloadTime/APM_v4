2fb1196fe92e833a78a2bbce9f7f78fc07fa32db
realReloadTime <max.shilcko@yandex.ru>
2025-07-24 23:00:10 +0700

    Выполнен SubsystemStatus Service с CRUD функционалом

--- core/logic/subsystem_status.py
+++ core/logic/subsystem_status.py
@@ -1,15 +1,10 @@
 from asgiref.sync import sync_to_async
+
 from django.db.models import QuerySet
-
-from ..models import SubsystemStatus
-
+from django.http import JsonResponse
 from rest_framework.serializers import ModelSerializer
 
-
-class SubsystemStatusSerializer(ModelSerializer):  # ExampleSerializer(example) -> JSON response
-    class Meta:
-        model = SubsystemStatus
-        fields = '__all__'
+from ..models import SubsystemStatus
 
 
 class SubsystemStatusRepository:  # CRUD логика чистой работы с БД
@@ -26,20 +21,42 @@ class SubsystemStatusRepository:  # CRUD логика чистой работы 
 
     @staticmethod
     async def update_subsystem_status(data: dict) -> SubsystemStatus | None:
-        if 'id' not in data:
-            raise ValueError("Can't update without ID key.")
         updated = await SubsystemStatus.objects.filter(id=data['id']).aupdate(**data)
         if not updated:
             return None
         return await SubsystemStatus.objects.aget(id=data['id'])
 
-
     @staticmethod
     async def delete_subsystem_status(pk: int) -> True | False:
         result = await SubsystemStatus.objects.filter(id=pk).adelete()
-        return bool(result[0])
+        return bool(result)
 
 
 class SubsystemStatusService:  # бизнес-логика (связь между View и PostgreSQL)
+    class SubsystemStatusSerializer(ModelSerializer):  # ExampleSerializer(example) -> JSON response
+        class Meta:
+            model = SubsystemStatus
+            fields = '__all__'
+
     def __init__(self, repository: SubsystemStatusRepository):
         self.repository = repository
+
+    async def create_subsystem_status(self, data: dict) -> JsonResponse:
+        result = await self.repository.create_subsystem_status(data)
+        return await self.serialize_subsystem_status(result)
+
+    async def get_subsystem_status(self, pk: int | None) -> JsonResponse:
+        result = await self.repository.get_subsystem_status(pk)
+        return await self.serialize_subsystem_status(result)
+
+    async def update_subsystem_status(self, data: dict) -> JsonResponse:
+        if 'id' not in data:
+            raise ValueError("Can't update without ID key.")
+        result = await self.repository.update_subsystem_status(data)
+        return await self.serialize_subsystem_status(result)
+
+    async def delete_subsystem_status(self, pk: int) -> bool:
+        return await self.repository.delete_subsystem_status(pk)
+
+    async def serialize_subsystem_status(self, current_object: SubsystemStatus) -> JsonResponse:
+        return JsonResponse(self.SubsystemStatusSerializer(current_object).data)
