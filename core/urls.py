c1cf283b8b7d04feb913655cfeb6a976aa66a54b
realReloadTime <max.shilcko@yandex.ru>
2025-08-03 23:17:29 +0700

    Добавил забытые paths из предыдущего коммита

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from core.views.user import register, login, user_list, user_detail, user_update, user_delete
 from core.views.subsystem_status import SubsystemStatusCRUD
 
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:user_id>/', user_detail, name='user-detail'),
+    path('users/<int:user_id>/update/', user_update, name='user-update'),
+    path('users/<int:user_id>/delete/', user_delete, name='user-delete'),
     path('subsystem_status/', SubsystemStatusCRUD.as_view(), name='subsystem-status-crud'),
 ]
