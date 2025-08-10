1c7170e74e25caee926899cfbf3302ee7f12c03d
Никита Сошкин <niksoshin8@gmail.com>
2025-08-10 18:37:20 +0700

    Начало загрузки данных при редактировании

---
+++ core/templates/core/js/loadEventData.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accessToken = localStorage.getItem('access_token');
+
+    function handleError(error, status) {
+        console.error('Ошибка:', error);
+        alert(`Произошла ошибка: ${error.message || status || 'Неизвестная ошибка'}`);
+    }
+
+    async function loadData(object, object_value, object_text){
+        try {
+            const response = await fetch(`${window.APP_CONFIG.API_BASE_URL}/api/${object}/`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` }
+            });
+
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+            const data = await response.json();
+            renderObject(object, data, object_value, object_text);
+
+        } catch (error) {
+            handleError(error, error.status);
+        }
+    }
+
+    function renderObject(object, data, object_value, object_text) {
+    const objectSelect = document.getElementsByName(object);
+    if (!objectSelect) {
+        console.error('Element not found:', object);
+        return;
+    }
+    objectSelect.forEach(select => {
+            select.innerHTML = '';
+        })
+    if (data.length === 0) {
+        const option = document.createElement('option');
+        option.textContent = '--------';
+        objectSelect.forEach(select => {
+            select.appendChild(option);
+        })
+        return;
+    }
+
+    data.forEach(data_object => {
+        objectSelect.forEach(select => {
+            const option = document.createElement('option');
+            option.value = data_object[object_value || 'id'];
+            option.textContent = data_object[object_text || 'name'];
+            select.appendChild(option);
+        }
+        )
+    });
+}
+    loadData("loas", "id", "name");
+    loadData("locations", "id", "name");
+    loadData("categories", "id", "name");
+    loadData("systems", "id", "name");
+    loadData("subsystems", "id", "name");
+    loadData("subsystem_statuses", "id", "name");
+    loadData("conditions", "id", "name");
+    loadData("precipitations", "id", "name");
+    loadData("sources", "id", "name");
+});
--- core/templates/core/js/loadFormData.js
+++ core/templates/core/js/loadFormData.js
@@ -6,56 +6,78 @@ document.addEventListener('DOMContentLoaded', function() {
         alert(`Произошла ошибка: ${error.message || status || 'Неизвестная ошибка'}`);
     }
 
-    async function loadData(object, object_value, object_text){
+    function getEventIdFromUrl() {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('event_id');
+    }
+
+    async function loadData(event_id) {
         try {
-            const response = await fetch(`${window.APP_CONFIG.API_BASE_URL}/api/${object}/`, {
+            const response = await fetch(`${window.APP_CONFIG.API_BASE_URL}/api/events/${event_id}/`, {
                 headers: { 'Authorization': `Bearer ${accessToken}` }
             });
 
-            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-            const data = await response.json();
-            renderObject(object, data, object_value, object_text);
+            if (!response.ok) {
+                const error = new Error(`HTTP error! status: ${response.status}`);
+                error.status = response.status;
+                throw error;
+            }
+            
+            const base_data = await response.json();
+            const category_data = loadDataByEvent(event_id, base_data.category_id);
+
+            renderInformation(base_data, category_data);
 
         } catch (error) {
-            handleError(error, error.status);
+            handleError(error);
+        }
+    }
+    async function loadDataByEvent(event_id, category_id) {
+        try {
+            const categories = [
+            { key: 1, label: 'equipment_failures' },
+            { key: 2, label: 'adverse_weathers' },
+            { key: 3, label: 'fire_dangers' },
+            { key: 4, label: 'geological_dangers' },
+            { key: 5, label: 'hydrological_dangers' },
+            { key: 6, label: 'emergency_situations' },
+            { key: 7, label: 'other_dangers' }
+        ];
+        const category = categories.find(cat => cat.key === category_id);
+        const url = `${window.APP_CONFIG.API_BASE_URL}/api/${category.label}/by-event/${event_id}/`;
+            const response = await fetch(url, {
+                headers: { 'Authorization': `Bearer ${accessToken}` }
+            });
+
+            if (!response.ok) {
+                const error = new Error(`HTTP error! status: ${response.status}`);
+                error.status = response.status;
+                throw error;
+            }
+            
+            const data = await response.json();
+
+        return data
+
+        } catch (error) {
+            handleError(error);
         }
     }
 
-    function renderObject(object, data, object_value, object_text) {
-    const objectSelect = document.getElementsByName(object);
-    if (!objectSelect) {
-        console.error('Element not found:', object);
-        return;
-    }
-    objectSelect.forEach(select => {
-            select.innerHTML = '';
-        })
-    if (data.length === 0) {
-        const option = document.createElement('option');
-        option.textContent = '--------';
-        objectSelect.forEach(select => {
-            select.appendChild(option);
-        })
-        return;
+    function renderInformation(base_data, category_data) {
+        if (base_data.category_id === 1) {
+            
+        }
+        document.getElementById('consequences').value = base_data.consequences || '';
+        document.getElementById('loas').value = base_data.loa || '';
     }
 
-    data.forEach(data_object => {
-        objectSelect.forEach(select => {
-            const option = document.createElement('option');
-            option.value = data_object[object_value || 'id'];
-            option.textContent = data_object[object_text || 'name'];
-            select.appendChild(option);
-        }
-        )
-    });
-}
-    loadData("loas", "id", "name");
-    loadData("locations", "id", "name");
-    loadData("categories", "id", "name");
-    loadData("systems", "id", "name");
-    loadData("subsystems", "id", "name");
-    loadData("subsystem_statuses", "id", "name");
-    loadData("conditions", "id", "name");
-    loadData("precipitations", "id", "name");
-    loadData("sources", "id", "name");
+
+    const eventId = getEventIdFromUrl();
+    if (eventId) {
+        loadData(eventId);
+    } else {
+        console.error('Event ID not found in URL');
+        alert('Не указан идентификатор события в URL');
+    }
 });
